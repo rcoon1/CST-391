e02026d6f846b110d26e18e505d09c6f6790638f
rcoon1 <78291341+rcoon1@users.noreply.github.com>
2023-06-28 21:07:02 -0500

    update

---
+++ Topic 1/Activity 0/MusicAPI/src/app.ts
@@ -0,0 +1,19 @@
+import express, { Request, Response } from 'express';
+
+const app = express();
+
+const port = 3000;
+
+// Make sure you understand the following line of code.
+
+app.get('/', (req: Request, res: Response) => {
+
+res.send('Hello World from TypeScript!');
+
+});
+
+app.listen(port, () => {
+
+console.log(`Example app listening at http://localhost:${port}`)
+
+});
